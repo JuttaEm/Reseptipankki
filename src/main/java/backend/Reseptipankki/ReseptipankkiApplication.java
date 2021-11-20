d1fdc738bf5d552ea014fcf7884826474ff14b3c
JuttaEm <jutta.ruuskanen@gmail.com>
2021-11-20 11:43:16 +0200

    Added delete and edit functions

--- src/main/java/backend/Reseptipankki/ReseptipankkiApplication.java
+++ src/main/java/backend/Reseptipankki/ReseptipankkiApplication.java
@@ -30,10 +30,16 @@ public class ReseptipankkiApplication {
 			Category category1 = new Category("Vegan");
 			Category category2 = new Category("Soup");
 			Category category3 = new Category("TexMex");
+			Category category4 = new Category("Dessert");
+			Category category5 = new Category("Breakfast");
+			Category category6 = new Category("Asian");
 			
 			categoryRepository.save(category1);
 			categoryRepository.save(category2);
 			categoryRepository.save(category3);
+			categoryRepository.save(category4);
+			categoryRepository.save(category5);
+			categoryRepository.save(category6);
 			
 			
 			log.info("Save demodata");
@@ -41,6 +47,9 @@ public class ReseptipankkiApplication {
 			recipeRepository.save(new Recipe("Makaronilaatikko", "Loistava vegaaninen ruoka!", true, 4, category1));
 			recipeRepository.save(new Recipe("Kasvissosekeitto", "Maukasta talviruokaa", false, 3, category2));
 			recipeRepository.save(new Recipe("Tortillat", "Helppoa ja herkullista :)", true, 5, category3));
+			recipeRepository.save(new Recipe("Porkkanakakku", "Terveellinen vaihtoehto täytekakulle", true, 4, category4));
+			recipeRepository.save(new Recipe("Munakasrulla", "Yllättävän vaikea saada pysymään koossa", false, 2, category5));
+			recipeRepository.save(new Recipe("Paistettu riisi kanalla", "Superhelppo arkiruoka", true, 5, category6));
 			
 			log.info("Fetch all recipes");
 			for (Recipe recipe : recipeRepository.findAll()) {
--- src/main/java/backend/Reseptipankki/web/RecipeController.java
+++ src/main/java/backend/Reseptipankki/web/RecipeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,6 +44,19 @@ public class RecipeController {
 	public String save(Recipe recipe) {
 		recipeRepository.save(recipe);
 		return "redirect:recipelist";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteRecipe(@PathVariable("id") Long recipeId, Model model) {
+		recipeRepository.deleteById(recipeId);
+		return "redirect:../recipelist";
+	}
+	
+	@RequestMapping(value="/edit/{id}")
+	public String editRecipe(@PathVariable("id") Long recipeId, Model model) {
+		model.addAttribute("recipe", recipeRepository.findById(recipeId));
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "editrecipe";
 	}
 	
 	
